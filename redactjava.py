09171512d887826a2ab80da0d8a57edcb4e54698
ai-xiaihai <ai-xiaihai@users.noreply.github.com>
2017-01-08 18:44:27 +0800

    redo redactjava.py
    I encountered some difficulties dealing with multi-line comments, so I use a different way to approach.

--- redactjava.py
+++ redactjava.py
@@ -1,39 +1,62 @@
 # Redact a list of names in a java source code file
+# names would be turn into "NAME"
 # Author: Xinnan Cheng (2017 WT)
 # Usage: python redactjava <file with list of names> <original file> <redacted file>
 #
 
 import sys, re
 
-# redact a single-line comment, return a string
-def single(line, list):
-	sline = iline.split("//",1)
-	cline = sline[1] #comment part of the line
+# redact a string, return a string
+def redact(str, list):
+	#sline = iline.split("//",1)
+	#cline = sline[1] #comment part of the line
 	for name in list:
-		if re.search(name, line, re.IGNORECASE):
-			cline = re.compile(re.escape(name), re.IGNORECASE).sub("NAME", cline)
-	return sline[0] + "//" + cline
+		if re.search(name, str, re.IGNORECASE):
+			str = re.compile(re.escape(name), re.IGNORECASE).sub("NAME", str)
+	return str
 
 if __name__ == "__main__":
 
-	#load name list
+	# load name list
 	name_list = (open(sys.argv[1], "r")).read().splitlines()
 	
-	#redact the java file
+	# 0 means not comments, 1 means single line comments, 2 means multi-line comments
+	flag = 0
+	
+	# redact the java file
 	with open(sys.argv[2], "r") as fi, open(sys.argv[3], "w+") as fo:
 		#do something
-		for iline in fi:
-			if "//" in iline:
-				#single line comment
-				fo.write(single(iline,name_list))
-				
-			elif "/*" in iline:
-				1+1
-			else:
-				#replicate lines that contains no comments
-				fo.write(iline)
-	
-	#in_file = open( sys.argv[2], "r") 
-	#out_file = open( sys.argv[3], "w+") 
-	#in_file.close()
-	#out_file.close()
+		
+		while True:
+			c = fi.read(1)
+    		if not c:
+    			# EOF
+      			break
+      		elif c == '/':
+      			c2 = fi.read(1)
+      			if c2 == "/":
+      				# single line comment
+      				fo.write( "//" + redact(fi.readline(), name_list) )
+      				
+      			elif c2 == "*":
+      				# multi-line comment
+      				
+      			else:
+      				fo.write(c+c2)
+      		else:
+      			fo.write(c)
+      				
+    	print "Read a character:", c
+    	
+# 		for iline in fi:
+# 			if "//" in iline:
+# 				#single line comment
+# 				fo.write(single(iline,name_list))
+# 				
+# 			elif "/*" in iline:
+# 				if "*/" in iline:
+# 					
+# 			else:
+# 				#replicate lines that contains no comments
+# 				fo.write(iline)
+# 	
